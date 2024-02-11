218894e962031d78b8d169a1fc43fc2d305a6900
KshitijKapoor8 <kshitijkapoor8@gmail.com>
2024-02-10 20:45:26 -0600

    added routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NB() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="">STDHelp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NB;
---
+++ src/pages/Landing.js
@@ -0,0 +1,10 @@
+function Landing() {
+  return (
+    <div>
+      <h1>App</h1>
+      <p>App content</p>
+    </div>
+  );
+}
+
+export default Landing;
