7cccf258499e24ac5cda69740fae74b0b1fb9ba1
tommyh2o <tommyhuynh02n@gmail.com>
2024-02-13 18:11:23 -0600

    added model two to navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
-import NB from './components/Navbar';
-import Landing from './pages/Landing';
-import About from './pages/About';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NB from "./components/Navbar";
+import Landing from "./pages/Landing";
+import About from "./pages/About";
+import Prevalence from "./pages/ModelTwo/Prevalence";
 
 function App() {
-    return (
-        <Router>
-            <NB/>
-            <Routes>
-                <Route path="/home" element={<Landing />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <NB />
+      <Routes>
+        <Route path="/home" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/prevalence" element={<Prevalence />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/ModelTwo/Prevalence.js
@@ -0,0 +1,9 @@
+function Prevalence() {
+  return (
+    <div>
+      <h1>Prevalence Model Page</h1>
+      <p>Model 2</p>
+    </div>
+  );
+}
+export default Prevalence;
