ecfed735a9500d1e9493e94717cc243c3ba4dc1b
Huang Max <maxhuang@wireless-10-154-73-16.public.utexas.edu>
2024-02-13 22:29:37 -0600

    finish basic model 3 page

--- src/pages/Locator.js
+++ src/pages/Locator.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Card from "../components/LocatorCard";
+import ALPA from '../images/Model3/APLA.png';
+import AltaMed from '../images/Model3/AltaMed.png';
+import Torrance from '../images/Model3/Torrance.png';
 //import "../components/card.css";
 
 const Locator = () => {
@@ -10,23 +13,23 @@ const Locator = () => {
       ZipCode: "90036",
       Services: "HIV testing",
       Phone: "3232151725",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/57/Zahl_2003.jpg?20190929125532",
+      imageUrl: ALPA,
     },
     {
       title: "AltaMed Medical Group",
       Address: "5427 E Whittier Blvd Los Angeles, CA 90022",
       ZipCode: "90022",
-      Services: "STI testing and Treatment",
+      Services: "STI testing and Treatment, Mpox Vaccine",
       Phone: "3234006399, 8884999303 (Mpox Vaccine)",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/57/Zahl_2003.jpg?20190929125532",
+      imageUrl: AltaMed,
     },
     {
-      title: "ALPA Health",
-      Address: "5901 W Olympic Blvd Ste 310 Los Angeles",
-      ZipCode: "90036",
-      Services: "HIV testing",
-      Phone: "3232151725",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/57/Zahl_2003.jpg?20190929125532",
+      title: "Torrance Urgent Care",
+      Address: "2195 Sepulveda Blvd Torrance US, CA 90501",
+      ZipCode: "90501",
+      Services: "PEP",
+      Phone: "4243371600",
+      imageUrl: Torrance,
     },
   ];
 
