13b18858e9d1b5e4291f6bfd4cff3b746c186298
Huang Max <maxhuang@wireless-10-146-173-30.public.utexas.edu>
2024-02-15 14:20:50 -0600

    fixed home issue

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -12,7 +12,7 @@ function NB() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-          <Nav.Link as={Link} to="/home">Home</Nav.Link>
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/counties">Counties</Nav.Link>
             <Nav.Link as={Link} to="/locator">Locator</Nav.Link>
