5133f9fde1787c311c9446672616960ffb35e27d
KshitijKapoor8 <kshitijkapoor8@gmail.com>
2024-03-03 16:31:12 -0600

    endpoints progress, have to dockerize

--- back-end/. /dist/api/lib/routers/lib/main.js
+++ back-end/. /dist/api/lib/routers/lib/main.js
@@ -2,11 +2,8 @@ import { Hono } from "hono";
 import { prevalenceRouter } from "./prevalence/router.js";
 import { countyRouter } from "./county/router.js";
 import { treatmentCenterRouter } from "./treatment-center/router.js";
-
 export const mainRouter = new Hono()
-  .basePath("/api/v1")
-  .route("/prevalence", prevalenceRouter)
-  .route("/county", countyRouter)
-  .route("/treatment-center", treatmentCenterRouter);
-
-export type AppType = typeof mainRouter;
+    .basePath("/api/v1")
+    .route("/prevalence", prevalenceRouter)
+    .route("/county", countyRouter)
+    .route("/treatment-center", treatmentCenterRouter);
--- back-end/src/api/lib/routers/lib/county/router.ts
+++ back-end/src/api/lib/routers/lib/county/router.ts
@@ -1,11 +1,11 @@
 import { Hono } from "hono";
 import pool from "../../../../../db/index.js";
 
-export const prevalenceRouter = new Hono()
+export const countyRouter = new Hono()
   .get("/", async (c) => {
     try {
       const client = await pool.connect();
-      const res = await client.query("SELECT * FROM prevalence");
+      const res = await client.query("SELECT * FROM county");
       client.release();
       return c.json(res);
     } catch (err) {
@@ -16,8 +16,9 @@ export const prevalenceRouter = new Hono()
   .post("/", async (c) => {
     try {
       const { year, gender, cases, casesper100k } = await c.req.json();
+      console.log("INPUTS: ", year, gender, cases, casesper100k);
       const queryText =
-        "INSERT INTO prevalence(year, gender, cases, casesper100k) VALUES($1, $2, $3, $4) RETURNING *";
+        "INSERT INTO county(year, gender, cases, casesper100k) VALUES($1, $2, $3, $4)";
       const queryValues = [year, gender, cases, casesper100k];
       const client = await pool.connect();
       const res = await client.query(queryText, queryValues);
