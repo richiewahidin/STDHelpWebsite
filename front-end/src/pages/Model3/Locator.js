7f95c92f29dee5ec9cffdaee92c229a40e273caf
Huang Max <maxhuang@Huangs-MacBook-Pro.local>
2024-03-06 01:00:09 -0600

    pagination and json for model3

--- front-end/src/pages/Model3/Locator.js
+++ front-end/src/pages/Model3/Locator.js
@@ -1,54 +1,45 @@
-import React from "react";
-import Card from "../../components/LocatorCard";
-import ALPA from '../../images/Model3/APLA.png';
-import AltaMed from '../../images/Model3/AltaMed.png';
-import Torrance from '../../images/Model3/Torrance.png';
+import React, { useState } from "react";
+import LocatorCard from "../../components/LocatorCard";
+//import "./Loca.css";
+import LocatorData from "./LocatorData.json";
+import Pagination from "@mui/material/Pagination";
 
 const Locator = () => {
-  const cardData = [
-    {
-      title: "ALPA Health",
-      Address: "5901 W Olympic Blvd Ste 310 Los Angeles, 90036",
-      Website: "https://aplahealth.org/",
-      Services: "HIV testing",
-      Phone: "3232151725",
-      imageUrl: ALPA,
-      url: "/locator/ALPA",
-    },
-    {
-      title: "AltaMed Medical Group",
-      Address: "5427 E Whittier Blvd Los Angeles, 90022",
-      Website: "https://www.altamed.org/",
-      Services: "STI testing and Treatment, Mpox Vaccine",
-      Phone: "3234006399, 8884999303 (Mpox Vaccine)",
-      imageUrl: AltaMed,
-      url: "/locator/Alta",
-    },
-    {
-      title: "Torrance Urgent Care",
-      Address: "2195 Sepulveda Blvd Torrance US, 90501",
-      Website: "https://exerurgentcare.com/locations_blue/torrance-north/",
-      Services: "PEP",
-      Phone: "4243371600",
-      imageUrl: Torrance,
-      url: "/locator/Torrance",
-    },
-  ];
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+  const cardData = LocatorData.Locator;
+
+  // Find indexes of items to display
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  const handleChange = (event, value) => {
+    setCurrentPage(value);
+  };
+  const currentItems = cardData.slice(indexOfFirstItem, indexOfLastItem);
 
   return (
-    <div className="grid">
-      {cardData.map((item, index) => (
-        <Card
-          key={index}
-          title={item.title}
-          Address={item.Address}
-          Website={item.Website}
-          Services={item.Services}
-          Phone={item.Phone}
-          imageUrl={item.imageUrl}
-          url = {item.url}
-        />
-      ))}
+    <div className="container">
+      <h1>Locator</h1>
+      <div className="grid">
+        {/* Map over currentItems instead of cardData */}
+        {currentItems.map((item, index) => (
+          <PrevalenceCard key={index} {...item} />
+        ))}
+      </div>
+      <Pagination
+        count={Math.ceil(cardData.length / itemsPerPage)}
+        page={currentPage}
+        onChange={handleChange}
+        variant="outlined"
+        shape="rounded"
+        size="large"
+        className="pagination"
+      />
+      <div style={{ padding: 15, paddingBottom: 20 }}>
+        Displaying: {indexOfFirstItem + 1} -{" "}
+        {Math.min(indexOfLastItem, cardData.length)} out of {cardData.length}
+      </div>
     </div>
   );
 };
