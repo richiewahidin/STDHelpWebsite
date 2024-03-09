29a9cd0872faf552f5a7415843aad64e1445027c
Richie Wahidin <richie@Richies-MBP-2.lan>
2024-03-08 23:30:52 -0600

    used API for Model One data

--- front-end/src/pages/ModelOne/Counties.js
+++ front-end/src/pages/ModelOne/Counties.js
@@ -1,85 +1,68 @@
-import React, { useEffect } from "react";
-import { Card, Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
-import CountiesData from "./CountiesData.json";
+import { Card, Col, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import { Link, useNavigate} from 'react-router-dom';
 import Pagination from "@mui/material/Pagination";
-import "./Counties.css";
-import { useNavigate } from "react-router-dom";
+import './Counties.css';
 import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Counties = () => {
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [itemsPerPage] = React.useState(20);
-  const countyData = CountiesData.counties;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(9);
+    const [data, setData] = useState([]); // Initialize data state as an empty array
+    const nav = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://ec2-3-93-193-224.compute-1.amazonaws.com/api/v1/county"
+        );
+        setData(response.data.rows); // Update state with fetched data rows
+        console.log(response.data.rows);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = countyData.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
   const handleChange = (event, value) => {
     setCurrentPage(value);
   };
 
-  const nav = useNavigate();
-  useEffect(() => {
-    const headers = {
-      "Content-Type": "application/json",
-    };
-    // async function fetchData() {
-    //   await axios.get("http://localhost:3000/api/v1/county", { headers });
-    // }
-    // const res = fetchData();
-    //
-    // console.log(res);
-
-    axios
-      .get(
-        "http://ec2-3-93-193-224.compute-1.amazonaws.com/api/v1/prevalence",
-        { headers },
-      )
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  const handleClick = (id) => {
-    nav("/counties/" + id);
+  const handleClick = (id, name, population, ccases, gcases, scases, escases, tscases, udcases) => {
+    // Pass the object as state along with the URL
+    nav(`/counties/${name}`, { state: {id, name, population, ccases, gcases, scases, escases, tscases, udcases} });
   };
 
   return (
     <div className="container">
-      <div style={{ padding: "20px" }}>
+      <div style={{ padding: '20px' }}>
         <h1>Counties</h1>
       </div>
       <Row>
         {currentItems.map((item, index) => (
-          <Col key={index} className="d-flex justify-content-center">
-            <Card
-              style={{ width: "20rem" }}
-              onClick={() => handleClick(item.id)}
-            >
-              <Card.Img src={item.imageUrl} width="200" height="200" />
+          <Col key={index} className="d-flex justify-content-center" onClick={() => handleClick(item.id, item.name, item.population, item.ccases, item.gcases, item.scases, item.escases, item.tscases, item.udcases, item.mapurl, item.flagurl, item.countyseat)}>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img src={item.imgurl} width="200" height="200" />
               <Card.Body>
                 <Card.Title>{item.name}</Card.Title>
                 <Card.Text>
-                  <ListGroup className="list-group-flush">
+                  <ListGroup className='list-group-flush'>
                     <ListGroupItem>
-                      County Seat: {item.countySeat}
-                      <br />
-                      Population: {item.population}
-                      <br />
+                      County Seat: {item.countyseat}<br />
+                      Population: {item.population}<br />
                     </ListGroupItem>
                     <ListGroupItem>
-                      <strong>2021 cases:</strong>
-                      <br />
-                      &ensp; Chlamydia: {item.chlamydiaCases}
-                      <br />
-                      &ensp; Gonorrhea: {item.gonorrheaCases}
-                      <br />
-                      &ensp; Syphilis*: {item.pssCases}
-                      <br />
+                      <strong>2021 cases:</strong><br />
+                      &ensp; Chlamydia: {item.ccases}<br />
+                      &ensp; Gonorrhea: {item.gcases}<br />
+                      &ensp; Syphilis*: {item.scases}<br />
                     </ListGroupItem>
                   </ListGroup>
                 </Card.Text>
@@ -89,7 +72,7 @@ const Counties = () => {
         ))}
       </Row>
       <Pagination
-        count={Math.ceil(countyData.length / itemsPerPage)}
+        count={Math.ceil(data.length / itemsPerPage)}
         page={currentPage}
         onChange={handleChange}
         variant="outlined"
@@ -98,16 +81,11 @@ const Counties = () => {
         className="pagination"
       />
       <div style={{ padding: 15, paddingBottom: 20 }}>
-        Displaying: {indexOfFirstItem + 1} -{" "}
-        {Math.min(indexOfLastItem, countyData.length)} out of{" "}
-        {countyData.length}
+        Displaying: {indexOfFirstItem + 1} - {Math.min(indexOfLastItem, data.length)} out of {data.length}
       </div>
-      <p style={{ position: "relative", bottom: "0px" }}>
-        {" "}
-        *This refers to Primary and Secondary Syphilis.
-      </p>
+      <p style={{ position: 'relative', bottom: '0px' }}> *This refers to Primary and Secondary Syphilis.</p>
     </div>
   );
-};
+}
 
 export default Counties;
--- front-end/src/pages/ModelOne/CountiesInstance.js
+++ front-end/src/pages/ModelOne/CountiesInstance.js
@@ -1,53 +1,73 @@
 import React from 'react';
-import LocatorCard from "../../components/LocatorCard";
+import Card from "../../components/LocatorCard";
 import CountiesData from "./CountiesData.json";
 import { useParams } from 'react-router-dom';
-import LocatorData from '../Model3/LocatorData.json';
+import { useLocation } from "react-router-dom";
+import LocatorData from "../Model3/LocatorData";
 
 const CountiesInstance = () => {
-  let { id } = useParams();
-  const county = CountiesData.counties.find((county) => county.id === id);
-
-  if (!county) {
-    return <div>County not found</div>;
-  }
+    const location = useLocation();
+    const { id, name, population, ccases, gcases, scases, escases, tscases, udcases, mapurl, flagurl, countyseat} = location.state;
+  
+    // Create a prevalence instance
+    const instanceCounties = {
+      id: id,
+      name: name, 
+      population: population, 
+      ccases: ccases, 
+      gcases: gcases, 
+      scases: scases, 
+      escases: escases, 
+      tscases: tscases, 
+      udcases: udcases,
+      mapurl: mapurl,
+      flagurl: flagurl,
+      countyseat: countyseat
+    };
 
   return (
     <div style={{ position: 'relative', padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
         <div style={{ marginBottom: '20px' }}>
-            <h1><strong>{county.name} County</strong></h1>
+            <h1><strong>{instanceCounties.name} County</strong></h1>
 
             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', width: '100%' }}>
                 <div style={{ flex: '0 0 60%', marginBottom: '20px' }}>
-                    <h3 style={{ fontSize: '1.5rem', marginBottom: '10px' }}> County Seat: {county.countySeat} </h3>
-                    <h3 style={{ fontSize: '1.5rem', marginBottom: '10px' }}> Population: {county.population} </h3>
-                    <h3 style={{ fontSize: '1.5rem', marginBottom: '10px' }}> <strong>2021 Cases / Rate:</strong></h3>
-                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Chlamydia</strong>: {county.chlamydiaCases} / {county.chlamydiaRate} ({county.chlamydiaRank})</h4>
-                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Gonnorhea</strong>: {county.gonnorheaCases} / {county.gonnorheaRate} ({county.gonnorheaRank})</h4>
-                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Primary and Secondary Syphilis</strong>: {county.pssCases} / {county.pssRate} ({county.pssRank})</h4>
-                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Early non-primary non-secondary Syphilis</strong>: {county.enpnssCases} / {county.enpnssRate} ({county.enpnssRank})</h4>
-                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Total Early Syphilis</strong>: {county.tesCases} / {county.tesRate} ({county.tesRank})</h4>
-                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Unknown Duration or Late Syphilis</strong>: {county.udlssCases} / {county.udlssRate} ({county.enpnssRank})</h4>
+                    <h3 style={{ fontSize: '1.5rem', marginBottom: '10px' }}> County Seat: {instanceCounties.countyseat} </h3>
+                    <h3 style={{ fontSize: '1.5rem', marginBottom: '10px' }}> Population: {instanceCounties.population} </h3>
+                    <h3 style={{ fontSize: '1.5rem', marginBottom: '10px' }}> <strong>2021 Cases:</strong></h3>
+                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Chlamydia</strong>: {instanceCounties.ccases} </h4>
+                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Gonnorhea</strong>: {instanceCounties.gcases}</h4>
+                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Primary and Secondary Syphilis</strong>: {instanceCounties.scases} </h4>
+                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Early non-primary non-secondary Syphilis</strong>: {instanceCounties.escases} </h4>
+                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Total Early Syphilis</strong>: {instanceCounties.tscases} </h4>
+                    <h4 style={{ fontSize: '1rem', marginBottom: '5px' }}> <strong> Unknown Duration or Late Syphilis</strong>: {instanceCounties.udcases} </h4>
                 </div>
-                <div style={{ flex: '0 0 50%', maxWidth: '300px', position: 'relative' }}>
-                  <img src={county.flagUrl} style={{ width: '100%', height: 'auto' }}/>
-                </div>
-                <p style={{ fontSize: '1rem' }}> The numbers in the brackets are the relative rank of the county in rate of that particular STD.</p>
+                {/*<div style={{ flex: '0 0 50%', maxWidth: '300px', position: 'relative' }}>
+                  <img src={instanceCounties.flagurl} style={{ width: '100%', height: 'auto' }}/>
+          </div>*/}
             </div>
         </div>
-
-        {/* google maps embedded, responsive */}
+        {/*  google maps embedded, responsive
         <div style={{ marginBottom: '20px', clear: 'both' }}>
-            <iframe src={county.mapUrl} style={{ width: "100%", height: "400px", border: "0", allowfullscreen: "", loading: "lazy", referrerpolicy: "no-referrer-when-downgrade" }}></iframe>
-        </div>
-        <br></br>
+            <iframe src={instanceCounties.mapurl} style={{ width: "100%", height: "400px", border: "0", allowfullscreen: "", loading: "lazy", referrerpolicy: "no-referrer-when-downgrade" }}></iframe>
+        </div> 
+        <br></br> */}
 
         {/* Adds links to other instances from model 1 */}
         <h4> <strong> Locations in this county *(locations not correct for this phase): </strong></h4>
         <div className="grid" style={{alignContent:'flex-start', justifyContent: 'center'}} >
             {LocatorData.Locator.map((item, index) => (
-                <LocatorCard key={index} {...item} />
-            ))}
+                <Card
+                  key={index}
+                  title={item.title}
+                  Address={item.Address}
+                  ZipCode={item.ZipCode}
+                  Services={item.Services}
+                  Phone={item.Phone}
+                  imageUrl={item.imageUrl}
+                  url={item.url}
+                />
+                ))}
         </div>  
     </div>
   );
