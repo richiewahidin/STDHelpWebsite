3f8077fd94001ebfd8af58068f916132232a6520
tommyh2o <tommyhuynh02n@gmail.com>
2024-03-22 15:22:54 -0500

    graph for prevalence

--- front-end/src/pages/ModelTwo/InstancePrevalence.js
+++ front-end/src/pages/ModelTwo/InstancePrevalence.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Container, Image, Row, Col } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const InstancePrevalence = () => {
   const location = useLocation();
   const {
     // id,
-    // countyid,
+    countyid,
     year,
     sex,
     population,
@@ -19,6 +20,25 @@ const InstancePrevalence = () => {
     countyimage,
     countyName,
   } = location.state;
+
+  // Calculate the maximum value for scaling the chart
+  const maxCases = Math.max(c_cases, s_cases, g_cases);
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://d1ahbxgizovdow.cloudfront.net/county/${countyid}`
+        );
+        setData(response.data.rows); // Update state with fetched data rows
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Container className="mt-4">
@@ -38,7 +58,11 @@ const InstancePrevalence = () => {
           <Row className="mb-3">
             <Col>
               <div className="border p-3 mb-3">
-                <strong>Description:</strong> The {sex === "Total" ? "Male and Female" : sex} population in {countyName} during the year {year} was {population}. Below details the number of Chlamydia, Syphilis, and Gonorrhea cases for {sex} during this time.
+                <strong>Description:</strong> The{" "}
+                {sex === "Total" ? "Male and Female" : sex} population in{" "}
+                {countyName} during the year {year} was {population}. Below
+                shows the number of Chlamydia, Syphilis, and Gonorrhea cases
+                for {sex === "Total" ? "Males and Female" : sex}s during this time.
               </div>
             </Col>
           </Row>
@@ -48,7 +72,8 @@ const InstancePrevalence = () => {
                 <Col md={4}>
                   <Card>
                     <Card.Body>
-                      <strong>Chlamydia cases:</strong><br />
+                      <strong>Chlamydia cases:</strong>
+                      <br />
                       {c_cases} ({c_rate} cases per 100,000 people)
                     </Card.Body>
                   </Card>
@@ -56,7 +81,8 @@ const InstancePrevalence = () => {
                 <Col md={4}>
                   <Card>
                     <Card.Body>
-                      <strong>Syphilis cases:</strong><br />
+                      <strong>Syphilis cases:</strong>
+                      <br />
                       {s_cases} ({s_rate} cases per 100,000 people)
                     </Card.Body>
                   </Card>
@@ -64,7 +90,8 @@ const InstancePrevalence = () => {
                 <Col md={4}>
                   <Card>
                     <Card.Body>
-                      <strong>Gonorrhea cases:</strong><br />
+                      <strong>Gonorrhea cases:</strong>
+                      <br />
                       {g_cases} ({g_rate} cases per 100,000 people)
                     </Card.Body>
                   </Card>
@@ -72,6 +99,77 @@ const InstancePrevalence = () => {
               </Row>
             </Col>
           </Row>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "flex-end",
+              height: "300px",
+              margin: "20px 0",
+            }}
+          >
+            <div
+              style={{
+                width: "100px",
+                height: `${(c_cases / maxCases) * 100}%`,
+                backgroundColor: "blue",
+                margin: "0 10px",
+              }}
+            ></div>
+            <div
+              style={{
+                width: "100px",
+                height: `${(s_cases / maxCases) * 100}%`,
+                backgroundColor: "red",
+                margin: "0 10px",
+              }}
+            ></div>
+            <div
+              style={{
+                width: "100px",
+                height: `${(g_cases / maxCases) * 100}%`,
+                backgroundColor: "green",
+                margin: "0 10px",
+              }}
+            ></div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "flex-end",
+              height: "30px",
+              margin: "20px 0",
+            }}
+          >
+            <div style={{ width: "100px" }}>
+              Chlamydia
+              <br />
+              {c_cases}
+            </div>
+            <div style={{ width: "145px" }}>
+              Syphilis
+              <br />
+              {s_cases}
+            </div>
+            <div style={{ width: "100px" }}>
+              Gonorrhea
+              <br />
+              {g_cases}
+            </div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "flex-end",
+              height: "50px",
+              margin: "20px 0",
+            }}
+          >
+            <h5>Related Content</h5>
+          </div>
         </Card.Body>
       </Card>
     </Container>
